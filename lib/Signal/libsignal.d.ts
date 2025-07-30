03707825b8ee08529c681025b6fd4cc267b0b4ae
Raidens-1 <raidenstore99@gmail.com>
2025-07-30 11:12:51 +0700

    Add files via upload

--- lib/Signal/libsignal.d.ts
+++ lib/Signal/libsignal.d.ts
@@ -1,1 +1,4 @@
+import { SignalAuthState } from '../Types'
+import { SignalRepository } from '../Types/Signal'
 
+export declare function makeLibSignalRepository(auth: SignalAuthState): SignalRepository
